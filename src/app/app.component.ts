48636f9a46eb97e82b04776c0d4a081f6ff1a38d
Aurangzeb Husain <aurangzebhusain786@gmail.com>
2021-06-18 10:46:57 +0530

    Learned how to read form values || and resetting the form || patching the values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,44 @@ export class AppComponent {
   defaultQuestion= "pet";
   answer='';
   genders=['male','female'];
+  user={
+    username:'',
+    email:'',
+    secretQuestion:'',
+    answer:'',
+    gender:''
+  }
+  submitted: boolean;
 
 // onSubmit(form:NgForm){
 // console.log(form);
 // }
-
+suggestUserName(){
+  const suggestedName='Superuser';
+  // this.signupForm.setValue({
+  //   userData:{
+  //     username:suggestedName,
+  //     email:''
+  //   },
+  //   secret:'pet',
+  //   answer:'',
+  //   gender:'male'
+  // });
+  this.signupForm.form.patchValue({
+    userData:{
+      username:suggestedName
+    }
+  })
+}
 onSubmit(){
+  console.log(this.defaultQuestion);
 console.log(this.signupForm);
+this.submitted=true;
+this.user.username=this.signupForm.value.userData.username;
+this.user.email=this.signupForm.value.userData.email;
+this.user.secretQuestion=this.signupForm.value.secret;
+this.user.answer=this.signupForm.value.answer;
+this.user.gender=this.signupForm.value.gender;
+this.signupForm.reset();
 }
 }
