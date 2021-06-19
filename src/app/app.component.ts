987a7ea49008d5bb2075c98712f5852e9cc6e4e8
Aurangzeb Husain <aurangzebhusain786@gmail.com>
2021-06-19 10:29:42 +0530

    Reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,22 @@
-import { Component,ElementRef,ViewChild } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { Component,ElementRef,OnInit } from '@angular/core';
+import {FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  @ViewChild('f') signupForm:NgForm;
-  title = 'angular-workshop';
-  defaultQuestion= "pet";
-  answer='';
+export class AppComponent implements OnInit {
   genders=['male','female'];
-  user={
-    username:'',
-    email:'',
-    secretQuestion:'',
-    answer:'',
-    gender:''
-  }
-  submitted: boolean;
 
-// onSubmit(form:NgForm){
-// console.log(form);
-// }
-suggestUserName(){
-  const suggestedName='Superuser';
-  // this.signupForm.setValue({
-  //   userData:{
-  //     username:suggestedName,
-  //     email:''
-  //   },
-  //   secret:'pet',
-  //   answer:'',
-  //   gender:'male'
-  // });
-  this.signupForm.form.patchValue({
-    userData:{
-      username:suggestedName
-    }
-  })
+  signUpForm:FormGroup;
+
+  ngOnInit(){
+    this.signUpForm=new FormGroup({
+      username:new FormControl('',[Validators.required]),
+      email:new FormControl(null,[Validators.required,Validators.email]),
+      gender:new FormControl('male',[Validators.required]),
+    });
 }
-onSubmit(){
-  console.log(this.defaultQuestion);
-console.log(this.signupForm);
-this.submitted=true;
-this.user.username=this.signupForm.value.userData.username;
-this.user.email=this.signupForm.value.userData.email;
-this.user.secretQuestion=this.signupForm.value.secret;
-this.user.answer=this.signupForm.value.answer;
-this.user.gender=this.signupForm.value.gender;
-this.signupForm.reset();
+
 }
-}
+
